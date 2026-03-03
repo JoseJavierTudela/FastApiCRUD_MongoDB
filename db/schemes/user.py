a7c6822b45f1a033ea08e1665991966ed9ca7c21
jjtudela <jj.tudela18@gmail.com>
2026-03-03 10:14:35 +0100

    API local terminada

--- db/schemes/user.py
+++ db/schemes/user.py
@@ -4,6 +4,9 @@ def user_schema(user) -> dict:
         "username": user["username"],
         "email": user["email"]
     }
+
+def users_schema(users) -> list:
+    return [user_schema(user) for user in users]   
  
 
 
--- routers/users_db.py
+++ routers/users_db.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
-from db.schemes.user import user_schema
+from db.schemes.user import user_schema, users_schema
 from db.client import db_client
+from bson import ObjectId
 
 
 router = APIRouter(prefix="/userdb", 
@@ -9,12 +10,12 @@ router = APIRouter(prefix="/userdb",
                    responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}})
 
 
-def search_user(field: str, value: str):
+def search_user(field: str, key):
     """Search for a user in the database by field name and value.
     Returns a `User` instance or `None`.
     """
     try:
-        record = db_client.users.find_one({field: value})
+        record = db_client.users.find_one({field: key})
         if record:
             return User(**user_schema(record))
         return None
@@ -37,3 +38,37 @@ def create_user(user: User):
     new_user = user_schema(record) if record else None
 
     return User(**new_user)  
+
+@router.get("/", response_model=list[User])
+async def users():
+    return users_schema(db_client.users.find())
+
+@router.get("/{id}")  # Path
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
+
+@router.get("/")  # Query
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def user(id: str):
+
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
+
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+    
+@router.put("/", response_model=User)
+async def user(user: User):
+
+    user_dict = dict(user)
+    del user_dict["id"]
+
+    try:
+        db_client.users.find_one_and_replace(
+            {"_id": ObjectId(user.id)}, user_dict)
+    except:
+        return {"error": "No se ha actualizado el usuario"}
+
+    return search_user("_id", ObjectId(user.id))
